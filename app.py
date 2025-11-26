c7dbb8c76cb742a2dd92195b59aae99b9576e0f3
chihping <137780598+waterMonsterr@users.noreply.github.com>
2025-11-26 13:03:50 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ import random
 from langchain_groq import ChatGroq
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
-from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 from dotenv import load_dotenv
+from langchain.chains import create_retrieval_chain
 
 # --- 1. 基礎設定 ---
 load_dotenv()
